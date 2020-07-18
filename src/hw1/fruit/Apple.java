167d0188ef1f7a685be7e1afa8778353df53cf82
Elusios <EDM.Elusios@gmail.com>
2020-07-18 05:52:40 +0400

    Add HW1

---
+++ src/hw1/fruit/Apple.java
@@ -0,0 +1,13 @@
+package hw1.fruit;
+
+public class Apple implements Weight {
+    private float weight;
+
+    public Apple() {
+        this.weight = 1.0f;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
